64e554d1d19b448aa45e359220468d210528be64
Isaiah123321 <84051071+Isaiah123321@users.noreply.github.com>
2021-05-19 14:13:57 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,141 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    Ghost.say(Sentence_6)
+    Pet_Cat.say(Sentence_7)
+    Knight.say(Sentence_8)
+    Ghost.say(Sentence_9)
+    Princess.say(Sentence_10)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Pet_Duck.say(Sentence_1)
+    Knight.say(Sentence_2)
+    Knight.say(Sentence_3)
+    Ghost.say(Sentence_4)
+    Knight.say(Sentence_5)
+})
+let Sentence_10 = ""
+let Sentence_9 = ""
+let Sentence_8 = ""
+let Sentence_7 = ""
+let Sentence_6 = ""
+let Sentence_5 = ""
+let Sentence_4 = ""
+let Sentence_3 = ""
+let Sentence_2 = ""
+let Sentence_1 = ""
+let Pet_Cat: Sprite = null
+let Ghost: Sprite = null
+let Princess: Sprite = null
+let Pet_Duck: Sprite = null
+let Knight: Sprite = null
+Knight = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+Pet_Duck = sprites.create(img`
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 b c 5 5 d 4 c . . 
+    . b b b b 5 5 5 b f d d 4 4 4 b 
+    . b d 5 b 5 5 b c b 4 4 4 4 b . 
+    . . b 5 5 b 5 5 5 4 4 4 4 b . . 
+    . . b d 5 5 b 5 5 5 5 5 5 b . . 
+    . b d b 5 5 5 d 5 5 5 5 5 5 b . 
+    b d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+Princess = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+Ghost = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Player)
+Pet_Cat = sprites.create(img`
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e . b f b 
+    f d d f d d f d d f . f d f 
+    f b d d b b d d 2 f . f d f 
+    . f 2 2 2 2 2 2 b b f f d f 
+    . f b d d d d d d b b d b f 
+    . f d d d d d b d d f f f . 
+    . f d f f f d f f d f . . . 
+    . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+scene.setBackgroundColor(1)
+Knight.setPosition(77, 50)
+Pet_Duck.setPosition(77, 20)
+Princess.setPosition(60, 110)
+Ghost.setPosition(77, 110)
+Pet_Cat.setPosition(9, 56)
+Sentence_1 = "Oh no lady princess has been kidnapped!!!"
+Sentence_2 = "We must go save her"
+Sentence_3 = "Ghost unhand my princess"
+Sentence_4 = "you will never be able to defeat me"
+Sentence_5 = "oh really? I have your weakness"
+Sentence_6 = "NOOO NOT THE CAT"
+Sentence_7 = "just my presence fears you ghost"
+Sentence_8 = "Now unhand her ghost"
+Sentence_9 = "OK OK IM SORRY"
+Sentence_10 = "Thank you my knight"
